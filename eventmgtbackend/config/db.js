7b48dfe6ea8160813e8468b68466f9153a79d3a4
Nakul Makode <134138075+NAKULMAK05@users.noreply.github.com>
2025-02-25 17:10:32 +0530

    Update db.js

--- eventmgtbackend/config/db.js
+++ eventmgtbackend/config/db.js
@@ -1,14 +1,22 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://Nakul:Makode@event.mxgq6.mongodb.net/", {
+    const mongoURI = process.env.MONGO_URI;
+    if (!mongoURI) {
+      throw new Error("MONGO_URI environment variable not set");
+    }
+
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("MongoDB Connected");
   } catch (err) {
-    console.error(err.message);
+    console.error("Error connecting to MongoDB:", err.message);
     process.exit(1);
   }
 };
