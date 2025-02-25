114f443c97b0571bd3cf3d71b650e3934cf0d09e
Nakul Makode <134138075+NAKULMAK05@users.noreply.github.com>
2025-02-25 23:39:20 +0530

    Update OrganizerDashboard.jsx

--- event-management-frontend/src/components/Dashboard/OrganizerDashboard.jsx
+++ event-management-frontend/src/components/Dashboard/OrganizerDashboard.jsx
@@ -28,7 +28,7 @@ const OrganizerDashboard = () => {
   const [commentReply, setCommentReply] = useState({});
   const [filter, setFilter] = useState("all");
   const navigate = useNavigate();
-  const API_BASE_URL = import.meta.env.REACT_APP_API_BASE_URL
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
   useEffect(() => {
     fetchEvents();
     fetchUserProfile();
