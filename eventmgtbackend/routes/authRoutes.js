e51ee471cedacc9beaae4c28159978b09867ae34
Nakul Makode <134138075+NAKULMAK05@users.noreply.github.com>
2025-03-04 23:51:10 +0530

    Update authRoutes.js

--- eventmgtbackend/routes/authRoutes.js
+++ eventmgtbackend/routes/authRoutes.js
@@ -3,10 +3,10 @@ import { registerUser, loginUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
-// POST request to register a user
+// signup route
 router.post('/register', registerUser);
 
-// POST request to login a user
+// login route
 router.post('/login', loginUser);
 
 export default router;
