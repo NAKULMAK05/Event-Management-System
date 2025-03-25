1d9b8b4e206c23ffb3c4f8b94967c15aab234efe
Nakul Makode <134138075+NAKULMAK05@users.noreply.github.com>
2025-03-25 22:17:39 +0530

    Update Signup.jsx

--- event-management-frontend/src/components/Auth/Signup.jsx
+++ event-management-frontend/src/components/Auth/Signup.jsx
@@ -17,7 +17,7 @@ export default function SignupPage() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const navigate = useNavigate();
-
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
   const handleChange = ({ currentTarget: input }) => {
     setData({ ...data, [input.name]: input.value });
   };
